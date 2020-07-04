dd0182da1e0e213ed1a81d3dbe1fbeb12fd92233
code-rgb <62891774+code-rgb@users.noreply.github.com>
2020-07-04 11:05:53 +0530

    Change Logic a bit

--- userge/plugins/fun/year_progress.py
+++ userge/plugins/fun/year_progress.py
@@ -1,12 +1,16 @@
 #by @deleteduser420
 import datetime
 from userge import userge
+import math
 
 @userge.on_cmd("yp$", about={'header': "Year Progress Bar"})
 async def progresss(message):
     x = datetime.datetime.now()
     day = int(x.strftime("%j"))
-    percent = round( day / 365 * 100 )
+    def truncate(number, digits) -> float:
+        stepper = 10.0 ** digits
+        return math.trunc(stepper * number) / stepper
+    percent = truncate(( day / 365 * 100 ), 0)
     num = round(percent/5)
     
     progress = [
@@ -32,6 +36,7 @@ async def progresss(message):
     "▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓░",
     "▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓",
     ]
+
     message_out  =   "<b>Year Progress</b>\n"
     message_out  += f"<code>{progress[num]} {percent}</code>"
     message_out  +=  "`%`"
